0f4a536e9be3a7055456175f75b40bc4b0a5a113
Oliver Ni <oliver.ni@gmail.com>
2019-09-26 00:13:29 -0700

    View xp of other users

--- commands/xp.js
+++ commands/xp.js
@@ -2,11 +2,21 @@ const Discord = require("discord.js");
 
 module.exports.run = (client, message, args) => {
 
-    const key = `${message.guild.id}-${message.author.id}`;
+    let user = message.author;
+
+    if (args.length > 0 && args[0].startsWith('<@') && args[0].endsWith('>')) {
+        let mention = args[0].slice(2, -1);
+		if (mention.startsWith('!')) {
+			mention = mention.slice(1);
+		}
+		user = client.users.get(mention);
+    }
+
+    const key = `${message.guild.id}-${user.id}`;
 
     const embed = new Discord.RichEmbed();
-    embed.setThumbnail(message.author.avatarURL);
-    embed.setAuthor(message.author.tag, message.author.avatarURL);
+    embed.setThumbnail(user.avatarURL);
+    embed.setAuthor(user.tag, user.avatarURL);
 
     const curLevel = client.points.get(key, "level");
     const curXP = client.points.get(key, "points");
