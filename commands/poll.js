613db7c5473fcd06fceb1844c1aee9b8d111a9c8
Oliver Ni <oliver.ni@gmail.com>
2019-10-27 22:27:55 -0700

    Change polls

--- commands/poll.js
+++ commands/poll.js
@@ -7,11 +7,12 @@ exports.run = async (client, message, args) => {
 
     let choices = args.join(" ").split("|").map(e => e.trim());
     const title = choices[0];
-    choices = choices.slice(1);
+    const description = choices[1];
+    choices = choices.slice(2);
     
     const embed = new Discord.RichEmbed();
     embed.setTitle(`**${title}**`);
-    embed.setDescription(choices.map((val, idx) => `${alphabet[idx]} ${val}`).join("\n"));
+    embed.setDescription(description + "\n\n" + choices.map((val, idx) => `${alphabet[idx]} ${val}`).join("\n"));
     embed.setFooter('LHS Anime Club Bot', client.user.avatarURL);
 
     const suggestion = await message.channel.send(embed);
