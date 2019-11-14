cb45b6420038c5e01fd653821c4657b26a7eb074
Oliver Ni <oliver.ni@gmail.com>
2019-11-13 22:23:57 -0800

    Add kiss

--- commands/fun/kiss.js
+++ commands/fun/kiss.js
@@ -5,7 +5,7 @@ const Entities = require("html-entities").AllHtmlEntities;
 const fetchImage = () => {
     return axios.get("https://api.tenor.com/v1/random", {
         params: {
-            q: "anime lick",
+            q: "anime kiss",
             limit: 1,
             key: "KZ7Z2GJE8MWL"
         }
@@ -15,21 +15,21 @@ const fetchImage = () => {
 }
 
 module.exports = {
-    name: "lick",
-    description: "Lick.",
+    name: "kiss",
+    description: "Kiss.",
     channelType: ["text"],
     usage: (client, message) => {
-        message.channel.send("Usage: `!lick [something]`")
+        message.channel.send("Usage: `!kiss [something]`")
     },
     execute: async (client, message, args) => {
         message.channel.startTyping();
         try {
             let img = await fetchImage();
             const embed = new Discord.RichEmbed().setImage(img).setColor(0xF1C40F);
-            if (args.length > 0) message.channel.send(`${message.author} licked ${args.join(" ")}`, embed);
+            if (args.length > 0) message.channel.send(`${message.author} kissed ${args.join(" ")}`, embed);
             else message.channel.send(embed);
         } catch (err) {
-            message.channel.send("Failed to lick.");
+            message.channel.send("Failed to kiss.");
             console.error(err);
         }
         message.channel.stopTyping();
